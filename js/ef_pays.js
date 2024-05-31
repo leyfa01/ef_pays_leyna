3a0620a4525aa46cfd12f114474ccdd20b5578fc
Leyfa <leynafeknous2003@gmail.com>
2024-05-31 16:17:25 -0400

    ef-LF ajout de limage mise en avant

--- js/ef_pays.js
+++ js/ef_pays.js
@@ -6,25 +6,11 @@
         un_pays.addEventListener("mousedown", function (e) {
             console.log("ff");
             le_pays = e.target.id;
-            url = `https://gftnth00.mywhc.ca/tim39/wp-json/wp/v2/posts?search=${le_pays}`;
+            url = `https://gftnth00.mywhc.ca/tim39/wp-json/wp/v2/posts?search=${le_pays}&_embed`;
             fetchUrl(url);
         })
     }
 
-    function fetchCategories() {
-        let url = `https://gftnth00.mywhc.ca/tim39/wp-json/wp/v2/categories`;
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(categorie => {
-                    dataCategories[categorie.id] = {nom: categorie.name, lien: categorie.link};
-                });
-            })
-            .catch(function (error) {
-                // Gérer les erreurs
-                console.error("Erreur lors de la récupération des données :", error);
-            });
-    }
 
     function fetchUrl(url) {
         fetch(url)
@@ -45,10 +31,8 @@
                 // Par exemple, extraire les titres des articles comme dans l'exemple précédent
                 data.forEach(function (article) {
                     let titre = article.title.rendered;
-                    // Fait un element a, pour le lien de la categorie
-
                     // Couper le contenu
-                    let contenu = article.content.rendered.split(" ").slice(0, 10).join(" ") + "..";
+                    let contenu = article.content.rendered.split(" ").slice(0, 30).join(" ") + "..";
                     // Creer la carte
                     let carte = document.createElement("div");
                     carte.classList.add("restapi__carte");
@@ -56,9 +40,12 @@
                     // Remplir la carte
                     carte.innerHTML = `
                     <div class="carte">
-                        <h4>${titre}</h4>
-                        ${contenu}</p>
-                        <a href="https://gftnth00.mywhc.ca/tim39/?p=${article.id}">En savoir plus</a>
+                        <h4><a href="https://gftnth00.mywhc.ca/tim39/?p=${article.id}">${titre}</a></h4>
+                        <div class="carte_description">
+                            <img src="${article._embedded['wp:featuredmedia']['0'].source_url}" alt="">
+                            ${contenu}</p>
+                        </div>
+                        
                     </div>
                     `;
                     restapi.appendChild(carte);
